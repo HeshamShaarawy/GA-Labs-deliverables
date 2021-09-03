45a6a6a9c411c38c9613b78715bb9d2460ff88a5
Hesham <hesham2go@outlook.com>
2021-09-03 11:19:44 -0400

    streamlined newFlights function and improved code formatting and updated comments

--- mongoose-flights/controllers/flights.js
+++ mongoose-flights/controllers/flights.js
@@ -1,26 +1,23 @@
 const Flight = require('../models/flight')
 const Ticket = require('../models/ticket');
 
+// pulling all flights data then sorting them based on departure date then sending the data to index page for display:
 function showAll(req,res){   
     Flight.find(function(err, flights){
         const sortFlights = flights.slice().sort((a, b) => a.departs - b.departs);
+        if(err) return console.log(err)
         res.render('flights/index', {sortFlights})
-    
     })
 }
 
-async function newFlights(req,res){
-    const newFlight = new Flight();
-    const dt = newFlight.departs;
-    // Format the date for the value attribute of the input
-    const departsDate = dt // .toISOString().slice(0, 16);
-    res.render('flights/new', {departsDate});
+//rendering the form page for user to create a new flight
+function newFlights(req,res){
+    res.render('flights/new');
 }
 
-
+// handling the data received for a new flight and saving it in the database 
 function create(req, res) {
     const newflight = new Flight(req.body);
-    
     newflight.save(function(err) {
       // one way to handle errors
       if (err) return res.render('flights/new');
@@ -30,23 +27,23 @@ function create(req, res) {
     });
 };
 
+// handling request to show the details of of a flight: pulling the flight data from DB and sending it to view of flight details
 function flightDetails(req,res) {
     Flight.findById(req.params.id, function(err,flight){
-                const destinations = flight.destinations.slice().sort((a, b) => a.arrival - b.arrival);
-                const tickets = Ticket.find({ flight: flight._id }, function (err, tickets){
-                    res.render('flights/details', { flight, destinations, tickets });
-            });
-        });        
+        const destinations = flight.destinations.slice().sort((a, b) => a.arrival - b.arrival);
+        const tickets = Ticket.find({ flight: flight._id }, function (err, tickets){
+            res.render('flights/details', { flight, destinations, tickets });
+        });
+    });        
 }
 
+//adding a destination to a flight: through handling the data received from the form at view : flights/detail 
 async function addDest(req,res){
     let flight = await Flight.findById(req.params.id)
-
     flight.destinations.push({
         airport: req.body.airport,
         arrival:  req.body.arrival
     })
-   
     await flight.save()
     res.redirect(`/flights/${flight.id}`)
 }
