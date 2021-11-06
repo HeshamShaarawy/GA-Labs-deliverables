f673021bb5c23d55839ceda178aa285d4c748eed
Hesham <hesham2go@outlook.com>
2021-11-06 15:57:23 -0400

    enhance html/css

--- wacky/main_app/views.py
+++ wacky/main_app/views.py
@@ -1,3 +1,4 @@
+from django.forms import Widget
 from .models import Widget
 from .forms import WidgetForm
 from django.views.generic.edit import DeleteView
@@ -10,18 +11,17 @@ from django.db.models import Sum
 def index (request):
     widget = Widget.objects.all()
     total_quantity = Widget.objects.all().aggregate(Sum('quantity'))
-    total_quantity_num = total_quantity['quantity__sum']
+    total_quantity_sum = total_quantity['quantity__sum']
     widget_form = WidgetForm()
     return render(request, 'widgets/index.html',{
         'widget': widget, 'widget_form': widget_form,
-        'total_quantity': total_quantity_num
+        'total_quantity': total_quantity_sum
   })
 
 def widget_create(request):
   form = WidgetForm(request.POST)
   if form.is_valid():
     new_widget = form.save(commit=False)
-    print(new_widget)
     new_widget.save()
   return redirect('/')
 
@@ -29,5 +29,6 @@ def widget_create(request):
 class WidgetDelete(DeleteView):
     model = Widget
     success_url = '/'
+
 
 
