60502a2fc2da6aae91490a38b967f7d477600454
Hesham <hesham2go@outlook.com>
2021-11-06 18:01:34 -0400

    add class based view for listing all widgets under '/cbv'

--- wacky/main_app/urls.py
+++ wacky/main_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from . import views
+from . import views 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('/create/', views.widget_create, name='widget_create' ),
-    path('/<int:pk>/delete/', views.WidgetDelete.as_view(), name='widget_delete'),
-
+    path('', views.index, name='index'),    
+    path('create/', views.widget_create, name='widget_create' ),
+    path('<int:pk>/delete/', views.WidgetDelete.as_view(), name='widget_delete'),
+    path('add/', views.WidgetCreateView.as_view(), name='widget_add'),
+    path('cbv/', views.WidgetIndex.as_view(), name='cbv_index'),
+   
 ]
--- wacky/main_app/views.py
+++ wacky/main_app/views.py
@@ -1,11 +1,35 @@
-from django.forms import Widget
+from django.forms import Widget, widgets
+from django.views.generic.base import TemplateView
 from .models import Widget
 from .forms import WidgetForm
-from django.views.generic.edit import DeleteView
+from django.views.generic import ListView
+from django.views.generic.edit import DeleteView, CreateView, FormView
 from django.shortcuts import redirect, render
 from django.db.models import Sum
 
 
+class WidgetCreateView(CreateView):
+    model =  Widget
+    
+
+class WidgetIndex(ListView):
+  model = Widget
+  template_name = 'widgets/index.html'
+  context_object_name = 'widget'
+  form_widget = WidgetForm()
+  def get_context_data(self, **kwargs):
+          # Call the base implementation first to get a context
+    context = super().get_context_data(**kwargs)
+         # Add in a QuerySet of all the books
+    context['widget_form'] = WidgetForm()
+    total_quantity = Widget.objects.all().aggregate(Sum('quantity'))
+    context['total_quantity_sum'] = total_quantity['quantity__sum']
+    return context
+  
+
+class WidgetDelete(DeleteView):
+    model = Widget
+    success_url = '/cbv'
 
 
 def index (request):
@@ -26,9 +50,8 @@ def widget_create(request):
   return redirect('/')
 
 
-class WidgetDelete(DeleteView):
-    model = Widget
-    success_url = '/'
 
-
+def widget_delete(request):
+  Widget.objects.filter(id=id).delete()
+  return redirect('/')
 
